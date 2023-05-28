217d038c963a5f86579798add5e67aa95a03980e
RanithSarkar <134868777+RanithSarkar@users.noreply.github.com>
2023-05-28 23:48:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function clicked() {
+               document.getElementsByClassName("manu")[0].style.right="0vw";
+           }
+           function clicked1() {
+               document.getElementsByClassName("manu")[0].style.right="-72vw";
+           }
+           document.getElementsByClassName("age2")[0].innerHTML=(new Date().getFullYear()-2005)+" years";    
+           document.getElementsByClassName("ft")[0].innerHTML="|| Created by Ranith Sarkar ||<br><br>Copyright © "+new Date().getFullYear();
