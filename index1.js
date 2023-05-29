31cf4c1e32ec4a9936020c8ef6888fc99802b5ae
RanithSarkar <134868777+RanithSarkar@users.noreply.github.com>
2023-05-29 17:03:30 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,6 @@
+function clicked() {
+               document.getElementsByClassName("manu")[0].style.right="0vw";
+           }
+           function clicked1() {
+               document.getElementsByClassName("manu")[0].style.right="-72vw";
+           }
